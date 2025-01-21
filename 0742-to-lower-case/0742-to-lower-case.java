9d5123b7551b034b7e1bfbab03bf774a9654fa64
Omurbek <114275896+shaiurbekuO@users.noreply.github.com>
2025-01-21 23:08:30 +0600

    Time: 0 ms (100%), Space: 41.6 MB (74.35%) - LeetHub

---
+++ 0742-to-lower-case/0742-to-lower-case.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String toLowerCase(String s) {
+      StringBuilder sb = new StringBuilder(); 
+        for (char c : s.toCharArray()) {       
+            if (c >= 'A' && c <= 'Z') {        
+                sb.append((char) (c + 32));   
+            } else {
+                sb.append(c);                 
+            }
+        }
+        return sb.toString();          
+    }
+}
